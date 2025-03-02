30648dfddde688d7fec02645db895fe3fda7fe41
sasakarma <sakashun.ps@gmail.com>
2025-03-02 23:47:55 +0900

    added gitsigns, which-key

--- init.lua
+++ init.lua
@@ -113,5 +113,29 @@ require("lazy").setup({
       { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Telescope buffers' },
       { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Telescope help tags' },
     },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
   }
 })
