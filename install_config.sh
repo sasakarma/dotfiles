88bfa1d58c724b8f938edb8cca418c3fc7d0b555
sasakarma <sakashun.ps@gmail.com>
2025-03-03 22:55:52 +0900

    use absolute path for symlink

--- install_config.sh
+++ install_config.sh
@@ -8,14 +8,14 @@ neovim_config_path="$HOME/.config/nvim"
 mkdir -p "$neovim_config_path"
 if [ ! -e "$neovim_config_path/init.lua" ]; then
   echo "$repo_path/init.lua -> $neovim_config_path/init.lua"
-  ln -s "$repo_path/init.lua" "$neovim_config_path/init.lua"
+  ln -s "$(readlink -f $repo_path/init.lua)" "$neovim_config_path/init.lua"
 else
   echo "$neovim_config_path/init.lua already exsits!"
 fi
 
 if [ ! -e "$HOME/.tmux.conf" ]; then
   echo "$repo_path/.tmux.conf -> $HOME/.tmux.conf"
-  ln -s "$repo_path/.tmux.conf" "$HOME/.tmux.conf"
+  ln -s "$(readlink -f $repo_path/.tmux.conf)" "$HOME/.tmux.conf"
 else
   echo "$HOME/.tmux.conf already exsits!"
 fi
