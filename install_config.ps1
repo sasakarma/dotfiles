e50524666341ee3f4a216c084e52139138ff6c9f
sasakarma <sakashun.ps@gmail.com>
2025-03-04 23:58:43 +0900

    fixed nvim config path for powershell

--- install_config.ps1
+++ install_config.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$repoPath = ".\" 
+$repoPath = ".\nvim" 
 $neovimConfigPath = "$env:LOCALAPPDATA\nvim"
 
 # シンボリックリンクを作成
@@ -8,9 +8,9 @@ if (-not (Test-Path $neovimConfigPath)) {
   New-Item -ItemType Directory -Path $neovimConfigPath
 }
 
-if (-not (Test-Path "$neovimConfigPath\init.lua")) {
-  echo "$repoPath\init.lua -> $neovimConfigPath\init.lua"
-  New-Item -ItemType SymbolicLink -Path "$neovimConfigPath\init.lua" -Target "$repoPath\init.lua"
+if (-not (Test-Path "$neovimConfigPath")) {
+  echo "$repoPath -> $neovimConfigPath"
+  New-Item -ItemType SymbolicLink -Path "$neovimConfigPath" -Target "$repoPath"
 } else {
-  echo "$neovimConfigPath\init.lua already exists!"
+  echo "$neovimConfigPath already exists!"
 }
